47e8ea95711e5f901b00fb975d032b15c691dfb7
SalemChacoff <77862771+SalemChacoff@users.noreply.github.com>
2025-12-17 23:03:15 -0300

    add subtraction function and update main output; adjust test coverage threshold

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+fn minus(left: u64, right: u64) -> u64 {
+    left - right
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", minus(2, 2));
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = minus(2, 2);
+        assert_eq!(result, 0);
+    }
 }
